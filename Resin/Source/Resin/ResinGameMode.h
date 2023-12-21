192ced110a9ffea9211c2193a9928a9b6b69b8e9
ajosan25 <97420031+ajosan25@users.noreply.github.com>
2023-12-20 20:23:20 -0800

    I'm batman

---
+++ Resin/Source/Resin/ResinGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ResinGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AResinGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AResinGameMode();
+};
+
+
+
