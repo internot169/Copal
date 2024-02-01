c3b2b5383541756749e1dfd13ba13974eb1d2378
ajosan25 <97420031+ajosan25@users.noreply.github.com>
2024-01-31 21:11:46 -0800

    Finished first person player movement

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
-    public RotationAxes axes = RotationAxes.MouseXAndY;
-    public float sensitivityX = 15F;
-    public float sensitivityY = 15F;
+    public Transform player;
+    
+    public float sensitivityX = 100F;
+    public float sensitivityY = 100F;
 
     public float minimumX = -360F;
     public float maximumX = 360F;
@@ -19,31 +19,22 @@ public class MouseLook : MonoBehaviour
 
     void Update()
     {
-        if (axes == RotationAxes.MouseXAndY)
-        {
-            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
-
-            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-        }
-        else if (axes == RotationAxes.MouseX)
-        {
-            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-        }
+        if (Input.GetKey(KeyCode.Escape))
+            Cursor.lockState = CursorLockMode.None;
         else
-        {
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+            Cursor.lockState = CursorLockMode.Locked;
 
-            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
-        }
+        float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY * Time.deltaTime;
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+        transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+        player.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX * Time.deltaTime, 0);
     }
 
     void Start()
     {
-        // Make the rigid body not change rotation
         if (GetComponent<Rigidbody>())
             GetComponent<Rigidbody>().freezeRotation = true;
     }
