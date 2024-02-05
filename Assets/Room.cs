2e192ab4941555225ee9baca6122f8adbb85a9e3
ajosan25 <97420031+ajosan25@users.noreply.github.com>
2024-02-04 20:31:39 -0800

    Nearby Warning System functional

---
+++ Assets/Room.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    // Spawn location, relative to the center of the room
+    public Transform spawnLocation;
+    public Teleporter[] doors;
+
+    public string info = "";
+
+    // TODO: Make an active room system
+    public void Awake(){
+        Debug.Log(adjacencyCheck());
+    }
+
+    public string adjacencyCheck(){
+        string adjacent = "";
+        for (int i = 0; i < doors.Length; i++)
+        {
+            if (doors[i].next.info != ""){
+                adjacent += doors[i].next.info;
+                adjacent += " ";
+            }
+        }
+        return adjacent;
+    }
+}
