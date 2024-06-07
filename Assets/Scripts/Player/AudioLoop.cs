0927f9c815704136b6f6a6273c5c03f1c60d1d6a
internot169 <97420031+internot169@users.noreply.github.com>
2024-06-07 16:32:55 -0700

    player scripts

--- Assets/Scripts/Player/AudioLoop.cs
+++ Assets/Scripts/Player/AudioLoop.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioLoop : MonoBehaviour {
+    // Clips to loop between
     public AudioClip[] clips;
     public AudioSource audioSource;
     void Start () {
         audioSource.loop = false;
     }
-
+    // Change to random song
     public AudioClip GetClip()
     {
         return clips [Random.Range (0, clips.Length)];
     }
 
     void Update () {
+        // If not playing, change to random song
         if (!audioSource.isPlaying)
         {
             Debug.Log("Picking");
