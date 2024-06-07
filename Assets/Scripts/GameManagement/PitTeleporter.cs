ab00f8c5f170ff05309ec50e7edc5cf5ecc245a4
Yile Shen <yshen@eastsideprep.org>
2024-06-07 15:33:30 -0700

    Update PitTeleporter.cs

--- Assets/Scripts/GameManagement/PitTeleporter.cs
+++ Assets/Scripts/GameManagement/PitTeleporter.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class PitTeleporter : Teleporter
 {
+    // hold a reference to the other collider.
     Collider otherCollider;
 
+    // recieves the correct answer from the trivia object. 
     public void receive(bool correct){
         Debug.Log("Received");
         Debug.Log(correct);
+        // if the answer is correct, send to the right room and treat normally. 
         if (correct){
             Room temp = base.next;
             // Go back to the beginning
@@ -19,20 +22,24 @@ public class PitTeleporter : Teleporter
 
             base.next = temp;
         } else {
+            // otherwise, lose a life. 
             GameObject.Find("GameManager").GetComponent<GameManager>().lose(true);
         }
     }
 
     public override void MovePlayer(Collider other)
-    {
+    {   
+        // hold reference to other collider. 
         otherCollider = other;
         StartCoroutine(ShowUI());
     }
 
+    // show the UI for pits for a bit
     IEnumerator ShowUI(){
         base.gameManager.PitUI.SetActive(true);
         yield return new WaitForSecondsRealtime(2f);
         base.gameManager.PitUI.SetActive(false);
+        // start the trivia. 
         StartCoroutine(StartTrivia());
     }
 
