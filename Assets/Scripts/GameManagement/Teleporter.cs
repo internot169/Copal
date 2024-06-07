3f1f32728c677cb89df84baf6ed2febf24e7c6b9
Yile Shen <yshen@eastsideprep.org>
2024-06-07 15:37:03 -0700

    Update Teleporter.cs

--- Assets/Scripts/GameManagement/Teleporter.cs
+++ Assets/Scripts/GameManagement/Teleporter.cs
@@ -15,12 +15,15 @@ public class Teleporter : PlayerCollider
     // should be changed to false when we have detection for room end. 
     public bool isOn = true;
 
+    // reference to the game manager. 
     public GameManager gameManager;
 
     public void Awake(){
+        // find and assign game manager. 
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
+    // assign the numbers that tell the destination of the room. 
     public void AssignNumbers(){
         TextMeshPro[] texts = GetComponentsInChildren<TextMeshPro>();
         foreach (TextMeshPro meshes in texts){
@@ -40,15 +43,18 @@ public class Teleporter : PlayerCollider
             // tell the gamemanager that you are teleporting off this door. 
             // not using messaging because I'm unfamiliar, since I come from java. 
             // I think its easier to update a pointer.
+            // custom handling for the boss and pit teleporters. 
             if (this is BossTeleporter || this is PitTeleporter){
                 gameManager.move(next.roomNum, false, this);
                 // Set inactive later
             } else {
+                // otherwise, move normally. 
                 gameManager.move(next.roomNum, true, this);
             }
         }
     }
 
+    // on the trigger enter, call interact player. 
     void OnTriggerEnter(Collider other)
     {
         InteractPlayer(other);
@@ -56,19 +62,11 @@ public class Teleporter : PlayerCollider
 
     // all teleporters deal with moving the player, so this will change the player and do everything. 
     public virtual void MovePlayer(Collider other){
-        // patch to make sure i stop clipping into the ground. 
-        // we should use a different transform so its cleaner.
-        // move the player to the room
-        // all rooms are pregenerated in the scene, easier to move around. 
-        // would like to point out that diff scenes are possible, but loading times would be much slower
-        // so in a sense, by using enclosed rooms we're utilizing unity's LOD system to save time. 
-        // the LOD system stops rendering anything not in the player's LOS, which means that the walls prevents other 
-        // rooms from being rendered, but means that we can save time on room changes when compared to scene.
-        // only issue being clipping into other rooms, but we generally handle that gracefully. 
-
+        // activate the next room. 
         next.gameObject.SetActive(true);
         // check the next rooms for obstacles
         next.adjacencyCheck();
+        // move the player to the spawn location. 
         other.transform.position = new Vector3(next.spawnLocation.position.x, next.spawnLocation.position.y, next.spawnLocation.position.z);
     }
 }
