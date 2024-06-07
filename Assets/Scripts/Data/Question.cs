8f52fd503e39d47b6745720044865e9a6b8f82db
internot169 <97420031+internot169@users.noreply.github.com>
2024-06-07 16:33:56 -0700

    Comment data files

--- Assets/Scripts/Data/Question.cs
+++ Assets/Scripts/Data/Question.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class Question
 {
+    // Hint for the question
     public string lorePrereq { get; set; }
+    // Question and answers
     public string question { get; set; }
     public string answer1 { get; set; }
     public string answer2 { get; set; }
     public string answer3 { get; set; }
     public string answer4 { get; set; }
+    // Answer index
     public int answer = 3;
+    // Answer choice array
     public string[] choices;
     public bool known = false;    
     public bool answered = false;
+    // Initialize array and randomize locations of answers
     public void setup(){
         this.choices = new string[]{answer1, answer2, answer3, answer4};
         int index = Random.Range(0, choices.Length);
