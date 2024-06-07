df3080d6baa68c1c41f7c3dc7f4b269599c4dd27
ajosan123 <ajosan@eastsideprep.org>
2024-06-07 16:10:13 -0700

    Update MovingPlatform.cs

--- Assets/Scripts/Traps/MovingPlatform.cs
+++ Assets/Scripts/Traps/MovingPlatform.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
+    //Speed of the platform
     public float speed = 3.0f;
+    //The end position
     public Transform endPos;
+    //The start position
     public Transform startPos;
 
     void Update()
     {
+        //How far the object move each update
         var step = speed * Time.deltaTime;
+        //Makes the platform move towards the end pos
         transform.position = Vector3.MoveTowards(transform.position, endPos.position, step);
 
+        //if it is close enough, it swaps the start and end pos and goes back to where it came from
         if (Vector3.Distance(transform.position, endPos.position) < 0.001f)
         {
             Transform holdData = startPos;
